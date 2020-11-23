824f1cb5cc0e811b63c1b1810051271ade7f5bc3
Ankit Sharma <ankit.kumar.cse19@itbhu.ac.in>
2020-11-24 03:01:56 +0530

    Minor fixes

--- HealthPlusPlus/settings.py
+++ HealthPlusPlus/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import chartkick
 import dj_database_url
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
