ccad444782570d217d61298adb78d9a4f8bf3384
Ankit Sharma <ankit.kumar.cse19@itbhu.ac.in>
2020-11-24 03:00:45 +0530

    Minor fixes

--- HealthPlusPlus/settings.py
+++ HealthPlusPlus/settings.py
@@ -77,6 +77,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'HealthPlusPlus.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
